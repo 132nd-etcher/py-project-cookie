6b1c200fd3209cdfcaef6c84cdb0cf835b7b1391
132nd-etcher <132nd-etcher@daribouca.net>
2018-06-17 09:40:39 +0200

    update doc conf

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ESST'
+project = '{{cookiecutter.project_slug}}'
 copyright = '2018, 132nd-etcher'
 author = '132nd-etcher'
 
@@ -125,7 +125,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ESSTdoc'
+htmlhelp_basename = '{{cookiecutter.project_slug}}doc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -152,7 +152,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ESST.tex', 'ESST Documentation',
+    (master_doc, '{{cookiecutter.project_slug}}.tex', '{{cookiecutter.project_slug}} Documentation',
      '132nd-etcher', 'manual'),
 ]
 
@@ -162,7 +162,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'esst', 'ESST Documentation',
+    (master_doc, '{{cookiecutter.project_slug}}', '{{cookiecutter.project_slug}} Documentation',
      [author], 1)
 ]
 
@@ -173,8 +173,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ESST', 'ESST Documentation',
-     author, 'ESST', 'One line description of project.',
+    (master_doc, '{{cookiecutter.project_slug}}', '{{cookiecutter.project_slug}} Documentation',
+     author, '{{cookiecutter.project_slug}}', '{{cookiecutter.project_short_description}}',
      'Miscellaneous'),
 ]
 
